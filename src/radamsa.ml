c1817cef016c7564de5a57a4822c694f3cf30d09
Jeong Jihoon <jjh_t@kaist.ac.kr>
2021-06-02 10:51:44 +0900

    Use Random.int32 for PRNG

--- src/radamsa.ml
+++ src/radamsa.ml
@@ -3,7 +3,7 @@ external c__radamsa : string -> int -> string = "caml__radamsa"
 
 let radamsa ?seed:(seed=0) input =
   match seed with
-  | 0 -> c__radamsa input (Random.int 1073741823)
+  | 0 -> c__radamsa input (Random.int32 (Int32.max_int) |> Int32.to_int)
   | _ -> c__radamsa input seed
 
 let () = 
