a403aa0f5de094766028d41236ea66bd2fbfe7a2
Jeong Jihoon <jjh_t@kaist.ac.kr>
2021-06-07 14:54:19 +0900

    Drop random numger generation for radamsa seed

--- src/radamsa.ml
+++ src/radamsa.ml
@@ -2,9 +2,7 @@ external c__init : unit -> unit = "caml__init"
 external c__radamsa : string -> int -> string = "caml__radamsa"
 
 let radamsa ?seed:(seed=0) input =
-  match seed with
-  | 0 -> c__radamsa input (Random.int32 (Int32.max_int) |> Int32.to_int)
-  | _ -> c__radamsa input seed
+  c__radamsa input seed
 
 let () = 
   c__init ();
